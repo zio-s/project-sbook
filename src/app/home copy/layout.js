67ac66742b5d64a81decb9137b5dad72a916c004
seming2 <popqr3@naver.com>
2024-11-23 14:52:56 +0900

    불필요한 page삭제

--- src/app/home copy/layout.js
+++
@@ -1,9 +0,0 @@
-import Footer from '@/components/layout/Footer';
-import Header from '@/components/layout/Header';
-import React from 'react';
-
-const HomeLayout = ({ children }) => {
-  return <>{children}</>;
-};
-
-export default HomeLayout;
